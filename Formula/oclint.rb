77aff4afcfa80cfc1c36623a531cc61b5a22993f
Björn Dahlgren <bjorn@dahlgren.at>
2018-08-07 14:20:07 +0200

    Add support for building HEAD

--- Formula/oclint.rb
+++ Formula/oclint.rb
@@ -6,15 +6,22 @@ class Oclint < Formula
   url "https://github.com/oclint/oclint/releases/download/v#{version}/oclint-#{version}-x86_64-darwin-17.4.0.tar.gz"
   head "https://github.com/oclint/oclint.git"
 
-  def install
-    clang_version = '5.0.1'
+  depends_on "cmake" => :build
+  depends_on "llvm@5" => :build
+  depends_on "ninja" => :build
 
-    include.install Dir['include/c++'] unless File.directory? "#{include}/c++"
-    "#{include}/c++".install Dir['include/c++/v1'] unless File.directory? "#{include}/c++/v1"
-    lib.install Dir['lib/clang'] unless File.directory? "#{lib}/clang"
-    "#{lib}/clang".install Dir['lib/clang/#{clang_version}'] unless File.directory? "#{lib}/clang/#{clang_version}"
-    lib.install Dir['lib/oclint']
-    bin.install Dir['bin/*']
+  def install
+    base_dir = '*'
+    if build.head?
+      Dir.chdir('oclint-scripts') {
+        system './build', "-llvm-root=#{Formula["llvm@5"].opt_prefix}", '-no-analytics', '-release', '-use-system-compiler'
+        system './bundle', "-llvm-root=#{Formula["llvm@5"].opt_prefix}", '-release'
+      }
+
+      base_dir = 'build/oclint-release/*'
+    end
+
+    prefix.install Dir[base_dir]
   end
 
   test do
